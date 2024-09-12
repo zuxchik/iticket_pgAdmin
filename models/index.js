afc473620bf16e3c1d995d8d56f4b35291e4b722
zuxchik <zuxriddin966@gmail.com>
2024-09-12 09:47:12 +0300

    alom yoq boldi

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 const Sequelize = require("sequelize")
 const sequelize = require("../config/database")
 
-const Admin_Moduls = require("./admin.model")(sequelize, Sequelize)
-const Ticket = require("./ticket.model")(sequelize, Sequelize)
+const Admin = require("./admin.model")(sequelize, Sequelize)
 const Booking = require("./booking.model")(sequelize, Sequelize)
+const Ticket = require("./ticket.model")(sequelize, Sequelize)
+const VenueType = require("./venue.photo.model")(sequelize, Sequelize)
+const VenuePhoto = require("./venue.type.model")(sequelize, Sequelize)
 
-module.exports = { Admin_Moduls, Booking, Ticket, sequelize }
+module.exports = { Admin, Booking, Ticket, VenueType, VenuePhoto, sequelize }
